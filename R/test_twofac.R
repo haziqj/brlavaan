725d2f055be2a9755c84dee664a63ca269815372
Haziq Jamil <haziq.jamil@ubd.edu.bn>
2024-08-22 20:42:33 +0800

    Add test for two factor models

--- R/test_twofac.R
+++ R/test_twofac.R
@@ -7,8 +7,8 @@ source("R/10-sem_rbm_functions.R")
 source("R/20-gen_data.R")
 
 # Reliability == 0.8 -----------------------------------------------------------
-dat <- gen_data_growth(n = 100, rel = 0.8, dist = "Normal")
-mod <- txt_mod_growth(0.8)
+dat <- gen_data_twofac(n = 100, rel = 0.8, dist = "Normal", lavsim = TRUE)
+mod <- txt_mod_twofac()
 
 fit_lav   <- sem(mod, dat)
 fit_ML    <- fit_sem(mod, dat, method = "ML")
@@ -17,8 +17,7 @@ fit_iRBM  <- fit_sem(mod, dat, method = "iRBM")
 fit_iRBMp <- fit_sem(mod, dat, method = "iRBMp")
 
 # Reliability == 0.5 -----------------------------------------------------------
-dat <- gen_data_growth(n = 100, rel = 0.5, dist = "Kurtosis")
-mod <- txt_mod_growth(0.5)
+dat <- gen_data_twofac(n = 100, rel = 0.5, dist = "Non-normal")
 
 fit_lav   <- sem(mod, dat)
 fit_ML    <- fit_sem(mod, dat, method = "ML")
