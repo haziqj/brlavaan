c0998e0d4deee697ecdf4968697c646c44185c9c
Haziq Jamil <haziq.jamil@ubd.edu.bn>
2024-08-29 05:52:26 +0800

    New sim results

--- R/30-simulations.R
+++ R/30-simulations.R
@@ -1,6 +1,9 @@
 library(tidyverse)
 library(lavaan)
+library(furrr)
 theme_set(theme_bw())
+source("R/10-sem_rbm_functions.R")
+source("R/20-gen_data.R")
 source("R/21-sim_functions.R")
 
 ncores <- parallel::detectCores() - 1
@@ -17,36 +20,37 @@ simu_id <-
   rownames_to_column(var = "simid")
 
 # Run simulations --------------------------------------------------------------
-# simu_res <- vector("list", length = nrow(simu_id))
-# for (i in seq_len(nrow(simu_id))) {
-#   dist  <- simu_id$dist[i]
-#   model <- simu_id$model[i]
-#   rel   <- simu_id$rel[i]
-#   n     <- simu_id$n[i]
-#
-#   cli::cli_inform("[{i} / {nrow(simu_id)}] Now running {model} models ({dist}) rel = {rel}, n = {n}\n")
-#   simu_res[[i]] <- sim_fun(dist, model, rel, n, lavsim = FALSE)
-#   cat("\n")
-# }
-#
-# results <-
-#   do.call(rbind, lapply(simu_res, \(x) x$simu_res)) |>
-#   mutate(
-#     method = factor(method, levels = c("ML", "eRBM", "iRBM", "iRBMp")),
-#     dist = factor(dist, levels = c("Normal", "Kurtosis", "Non-normal")),
-#     model = factor(model, labels = c("Growth model", "Two factor model")),
-#     rel = factor(rel, levels = c(0.8, 0.5), labels = c("Rel = 0.8", "Rel = 0.5")),
-#     n = factor(n)
-#   )
-# save(results, file = "R/sim_results.RData")
+simu_res <- vector("list", length = nrow(simu_id))
+for (i in seq_len(nrow(simu_id))) {
+  dist  <- simu_id$dist[i]
+  model <- simu_id$model[i]
+  rel   <- simu_id$rel[i]
+  n     <- simu_id$n[i]
+
+  cli::cli_inform("[{i} / {nrow(simu_id)}] Now running {model} models ({dist}) rel = {rel}, n = {n}\n")
+  simu_res[[i]] <- sim_fun(dist, model, rel, n, lavsim = FALSE)
+  cat("\n")
+}
+
+results <-
+  do.call(rbind, lapply(simu_res, \(x) x$simu_res)) |>
+  mutate(
+    method = factor(method, levels = c("ML", "eRBM", "iRBM", "iRBMp")),
+    dist = factor(dist, levels = c("Normal", "Kurtosis", "Non-normal")),
+    model = factor(model, labels = c("Growth model", "Two factor model")),
+    rel = factor(rel, levels = c(0.8, 0.5), labels = c("Rel = 0.8", "Rel = 0.5")),
+    n = factor(n)
+  )
+save(results, file = "R/sim_results.RData")
 
 # Analysis ---------------------------------------------------------------------
-load("R/sim_results.RData")
+# load("R/sim_results.RData")
 
 # Overall plot
 results |>
   drop_na() |>
   filter(abs(est - truth) / truth < 2) |>
+  # filter(method %in% c("iRBM", "ML")) |>
   group_by(dist, model, rel, n, method) |>
   summarise(bias = mean(est - truth, na.rm = TRUE)) |>
   ggplot(aes(x = as.numeric(n), y = abs(bias), col = method)) +
