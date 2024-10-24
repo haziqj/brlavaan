8dde4dbe5f800770168b702bfa1eff7fe8516711
Haziq Jamil <haziq.jamil@ubd.edu.bn>
2024-10-24 13:59:08 +0800

    Playing with the pack/unpack

---
+++ R/lav_model_estimate.R
@@ -0,0 +1,54 @@
+# lavaan:::lav_model_estimate
+# This function has the pack and unpack parameters (look at the objective_function() and gradient_function() functions)
+
+library(tidyverse)
+library(lavaan)
+library(tinytest)
+
+HS.model <- "
+  visual  =~ x1 + a*x2 + x3
+  textual =~ x4 + a*x5 + x6
+  speed   =~ x7 + a*x8 + x9
+"
+
+fit0 <- cfa(HS.model, data = HolzingerSwineford1939, do.fit = FALSE)
+theta.init <- coef(fit0)
+
+# extract some internal slots for housekeeping
+lavsamplestats <- fit0@SampleStats
+lavmodel       <- fit0@Model
+lavdata        <- fit0@Data
+lavoptions     <- fit0@Options
+lavpartable    <- fit0@ParTable
+lavcache       <- fit0@Cache
+
+tmp <- lavaan:::lav_model_estimate(
+    lavmodel = lavmodel,
+    lavpartable = lavpartable,
+    lavh1 = NULL,
+    lavsamplestats = lavsamplestats,
+    lavdata = lavdata,
+    lavoptions = lavoptions,
+    lavcache = lavcache,
+    start = "simple",
+    do.fit = TRUE
+  )
+
+# "unpack" to long version of the parameters
+x.unpack <- lav_model_get_parameters(lavmodel)
+parscale <- rep(1, length(x.unpack))
+z.unpack <- x.unpack * parscale
+
+# "pack" to make the short version of the parameters
+if (lavmodel@eq.constraints) {
+  z.pack <- as.numeric(
+    (z.unpack - lavmodel@eq.constraints.k0) %*% lavmodel@eq.constraints.K
+  )
+} else {
+  z.pack <- z.unpack
+}
+start.x <- z.pack
+
+
+
+
