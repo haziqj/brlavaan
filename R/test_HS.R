4f89401eeb9df8f4510e5ed7838fa9cc00e1e4c6
Haziq Jamil <haziq.jamil@ubd.edu.bn>
2024-10-24 16:32:08 +0800

    Update rbm functions
    Still something wrong with the standard errors

--- R/test_HS.R
+++ R/test_HS.R
@@ -13,20 +13,24 @@ HS.model <- "
 
 fit_lav   <- cfa(HS.model, HolzingerSwineford1939)
 fit_ML    <- fit_sem(HS.model, HolzingerSwineford1939, method = "ML")
-# fit_eRBM  <- fit_sem(HS.model, HolzingerSwineford1939, method = "eRBM")
-# fit_iRBM  <- fit_sem(HS.model, HolzingerSwineford1939, method = "iRBM")
-# fit_iRBMp <- fit_sem(HS.model, HolzingerSwineford1939, method = "iRBMp")
+fit_eRBM  <- fit_sem(HS.model, HolzingerSwineford1939, method = "eRBM")
+fit_iRBM  <- fit_sem(HS.model, HolzingerSwineford1939, method = "iRBM")
+fit_iRBMp <- fit_sem(HS.model, HolzingerSwineford1939, method = "iRBMp")
 
-# Test likelihood value --------------------------------------------------------
+# Packed version of coefficients
 with(get_lav_stuff(fit_lav), {
   z.unpack <- coef(fit_lav)
   if (lavmodel@eq.constraints) {
-    z.pack <- as.numeric(
+    z.pack <<- as.numeric(
       (z.unpack - lavmodel@eq.constraints.k0) %*% lavmodel@eq.constraints.K
     )
   } else {
-    z.pack <- z.unpack
+    z.pack <<- z.unpack
   }
+})
+
+# Test likelihood value --------------------------------------------------------
+with(get_lav_stuff(fit_lav), {
   loglik.val <<- loglik(
     theta = z.pack,
     lavmodel = lavmodel,
@@ -38,8 +42,7 @@ with(get_lav_stuff(fit_lav), {
 expect_equal(loglik.val, as.numeric(logLik(fit_lav)), tol = 1e-4)
 
 # Test maximum likelihood estimation -------------------------------------------
-est_lav <- coef(fit_lav)
-class(est_lav) <- "numeric"
+est_lav <- as.numeric(coef(fit_lav))
 est_ML <- coef(fit_ML)
 expect_equal(est_lav, est_ML, tol = 1e-4)
 
@@ -50,21 +53,19 @@ expect_equal(sd_lav, sd_ML, tol = 1e-4, check.attributes = FALSE)
 # Test gradient ----------------------------------------------------------------
 with(get_lav_stuff(fit_lav), {
   grad.lav <<- grad_loglik(
-    theta = coef(fit_lav),
+    theta = z.pack,
     lavmodel = lavmodel,
     lavsamplestats = lavsamplestats,
     lavdata = lavdata,
-    lavoptions = lavoptions,
-    idx = idx_constr
+    lavoptions = lavoptions
   )
   grad.num <<- numDeriv::grad(
     func = loglik,
-    x = coef(fit_lav),
+    x = z.pack,
     lavmodel = lavmodel,
     lavsamplestats = lavsamplestats,
     lavdata = lavdata,
-    lavoptions = lavoptions,
-    idx = idx_constr
+    lavoptions = lavoptions
   )
 })
 expect_equal(grad.lav, rep(0, length(grad.lav)), tol = 1e-4)
@@ -74,30 +75,27 @@ expect_equal(sign(grad.lav), sign(grad.num))
 # Test Hessian -----------------------------------------------------------------
 with(get_lav_stuff(fit_lav), {
   hessian.lav <<- hessian_loglik(
-    theta = coef(fit_lav),
+    theta = z.pack,
     lavmodel = lavmodel,
     lavsamplestats = lavsamplestats,
     lavdata = lavdata,
-    lavoptions = lavoptions,
-    idx = idx_constr
+    lavoptions = lavoptions
   )
   hessian.num1 <<- -numDeriv::hessian(
     func = loglik,
-    x = coef(fit_lav),
+    x = z.pack,
     lavmodel = lavmodel,
     lavsamplestats = lavsamplestats,
     lavdata = lavdata,
-    lavoptions = lavoptions,
-    idx = idx_constr
+    lavoptions = lavoptions
   )
   hessian.num2 <<- -numDeriv::jacobian(
     func = grad_loglik,
-    x = coef(fit_lav),
+    x = z.pack,
     lavmodel = lavmodel,
     lavsamplestats = lavsamplestats,
     lavdata = lavdata,
-    lavoptions = lavoptions,
-    idx = idx_constr
+    lavoptions = lavoptions
   )
 })
 expect_equal(hessian.lav, hessian.num1, tol = 1e-5)
@@ -106,20 +104,34 @@ expect_equal(hessian.lav, hessian.num2, tol = 1e-5)
 # Test cross product of scores -------------------------------------------------
 with(get_lav_stuff(fit_lav), {
   e_mat1 <<- first_order_unit_information_loglik(
-    theta = coef(fit_lav),
+    theta = z.pack,
     lavmodel = lavmodel,
     lavsamplestats = lavsamplestats,
     lavdata = lavdata,
-    lavoptions = lavoptions,
-    idx = idx_constr
+    lavoptions = lavoptions
   )
   e_mat2 <<- crossprod(scores_loglik(
-    theta = coef(fit_lav),
+    theta = z.pack,
     lavmodel = lavmodel,
     lavsamplestats = lavsamplestats,
     lavdata = lavdata,
-    lavoptions = lavoptions,
-    idx = idx_constr
+    lavoptions = lavoptions
   ))
 })
 expect_equal(e_mat1, e_mat2, tol = 1e-5)
+
+# Take a look at all methods ---------------------------------------------------
+tibble(
+  id = rep(seq_along(coef(fit_lav)), 4),
+  param = rep(names(coef(fit_lav)), 4),
+  est = c(fit_ML$coef, fit_eRBM$coef, fit_iRBM$coef, fit_iRBMp$coef),
+  se = c(fit_ML$stderr, fit_eRBM$stderr, fit_iRBM$stderr, fit_iRBMp$stderr),
+  method = rep(c("ML", "eRBM", "iRBM", "iRBMp"), each = length(coef(fit_lav))),
+  time = rep(c(fit_ML$time, fit_eRBM$time, fit_iRBM$time, fit_iRBMp$time),
+             each = length(coef(fit_lav)))
+) |>
+  pivot_wider(id_cols = c(id, param), names_from = method,
+              values_from = c(est, se)) |>
+  select(-id) |>
+  print(n = 100)
+
