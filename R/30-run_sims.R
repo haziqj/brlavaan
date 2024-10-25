90df0a56af9d74d597a79fd25ea1781d9f4bb20d
Haziq Jamil <haziq.jamil@ubd.edu.bn>
2024-10-25 16:47:25 +0800

    Add sim results

--- R/30-run_sims.R
+++ R/30-run_sims.R
@@ -37,74 +37,9 @@ results <-
   mutate(
     method = factor(method, levels = c("ML", "eRBM", "iRBM", "iRBMp")),
     dist = factor(dist, levels = c("Normal", "Kurtosis", "Non-normal")),
-    model = factor(model, labels = c("Growth model", "Two factor model")),
+    # model = factor(model, labels = c("Growth model", "Two factor model")),
     rel = factor(rel, levels = c(0.8, 0.5), labels = c("Rel = 0.8", "Rel = 0.5")),
     n = factor(n)
   )
 res_growth <- results
 save(res_growth, file = "R/sim_results_growth.RData")
-
-# Analysis ---------------------------------------------------------------------
-# load("R/sim_results_growth.RData")
-
-# Overall plot
-results |>
-  drop_na() |>
-  filter(abs(est - truth) / truth < 2) |>
-  # filter(method %in% c("iRBM", "ML")) |>
-  group_by(dist, model, rel, n, method) |>
-  summarise(bias = mean(est - truth, na.rm = TRUE, trim = 0.01)) |>
-  ggplot(aes(x = as.numeric(n), y = abs(bias), col = method)) +
-  geom_hline(yintercept = 0, linetype = "dashed", col = "gray30") +
-  geom_line(size = 0.8) +
-  scale_x_continuous(labels = c(15, 20, 50, 100, 1000)) +
-  facet_grid(model + rel ~ dist, scales = "free_y") +
-  labs(
-    x = "Sample size (n)",
-    y = "Absolute mean bias",
-    col = NULL
-  ) +
-  theme(legend.position = "top")
-
-# Two factor model results
-drop_na(results) |>
-  filter(model == "Two factor model", dist %in% c("Normal", "Non-normal")) |>
-  filter(param %in% c("y1~~y1", "fx~~fx", "fy~~fy", "fy~fx", "fx=~x2")) |>
-  filter(abs(est) < 5) |>
-  # filter(method != "iRBMp") |>
-  # filter(method != "iRBM") |>
-  group_by(param, dist, rel, n, method) |>
-  summarise(
-    bias = mean(est - truth, na.rm = TRUE, trim = 0.01),
-    truth = first(truth)
-  ) |>
-  mutate(
-    rel_bias = bias / truth,
-    param = factor(param, levels = c("y1~~y1", "fx~~fx", "fy~~fy", "fy~fx", "fx=~x2")),
-  ) |>
-  ggplot(aes(x = as.numeric(n), y = rel_bias, col = method)) +
-  geom_hline(yintercept = 0, linetype = "dashed", col = "gray30") +
-  geom_line() +
-  scale_x_continuous(labels = c(15, 20, 50, 100, 1000)) +
-  facet_grid(param ~ rel + dist, scales = "free_y") +
-  labs(
-    x = "Sample size (n)",
-    y = "Relative mean bias",
-    col = NULL
-  ) +
-  theme(legend.position = "top")
-
-results |>
-  filter(model == "Two factor model") |>
-  filter(param %in% c("y1~~y1", "fx~~fx", "fy~~fy", "fy~fx", "fx=~x2")) |>
-  filter(abs(est) < 5) |>
-  group_by(dist, rel, param, n, method) |>
-  summarise(
-    # bias = mean(est - truth, na.rm = TRUE),
-    mse = mean((est - truth) ^ 2, na.rm = TRUE),
-    # cover = mean((truth - 1.96 * se < est) & (est < truth + 1.96 * se), na.rm = TRUE),
-    # time = mean(time, na.rm = TRUE),
-    # nsimu = n()
-  ) |>
-  pivot_wider(names_from = n, values_from = mse, names_prefix = "n = ") |>
-  print(n = 100)
