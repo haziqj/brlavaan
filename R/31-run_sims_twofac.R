2b60d3d138400314c47a6e000a00dde8acc73c17
Haziq Jamil <haziq.jamil@ubd.edu.bn>
2024-11-04 14:19:33 +0800

    Reorganise simulation scripts

--- R/31-run_sims_twofac.R
+++ R/31-run_sims_twofac.R
@@ -1,25 +1,12 @@
-library(tidyverse)
-library(lavaan)
-library(furrr)
-theme_set(theme_bw())
-source("R/10-sem_rbm_functions.R")
-source("R/20-gen_data.R")
-source("R/21-sim_functions.R")
-
-ncores <- parallel::detectCores() - 2
-future::plan(multisession, workers = ncores)
-nsimu <- 1000
-
 simu_id <-
   expand_grid(
     dist = c("Normal", "Kurtosis", "Non-normal"),
-    model = c("twofac", "growth"),
+    model = c("twofac"),
     rel = c(0.8, 0.5),
     n = c(15, 20, 50, 100, 1000),
   ) |>
   rownames_to_column(var = "simid")
 
-# Run simulations --------------------------------------------------------------
 simu_res <- vector("list", length = nrow(simu_id))
 for (i in seq_len(nrow(simu_id))) {
   dist  <- simu_id$dist[i]
@@ -27,18 +14,27 @@ for (i in seq_len(nrow(simu_id))) {
   rel   <- simu_id$rel[i]
   n     <- simu_id$n[i]
 
-  cli::cli_inform("[{i} / {nrow(simu_id)}] Now running {model} models ({dist}) rel = {rel}, n = {n}\n")
-  simu_res[[i]] <- sim_fun(dist, model, rel, n, lavsim = FALSE)
+  cli::cli_inform(">>> {Sys.time()} <<<\n\n[{i} / {nrow(simu_id)}] Now running {model} models ({dist}) rel = {rel}, n = {n}\n")
+  simu_res[[i]] <- sim_fun(
+    dist = dist,
+    model = model,
+    rel = rel,
+    n = n,
+    lavsim = FALSE,
+    lavfun = "cfa",
+    meanstructure = FALSE,
+    bounds = "standard"
+  )
   cat("\n")
 }
 
-results <-
+res_twofac <-
   do.call(rbind, lapply(simu_res, \(x) x$simu_res)) |>
   mutate(
     method = factor(method, levels = c("ML", "eRBM", "iRBM", "iRBMp")),
     dist = factor(dist, levels = c("Normal", "Kurtosis", "Non-normal")),
-    model = factor(model, labels = c("Growth model", "Two factor model")),
+    # model = factor(model, labels = c("Growth model", "Two factor model")),
     rel = factor(rel, levels = c(0.8, 0.5), labels = c("Rel = 0.8", "Rel = 0.5")),
     n = factor(n)
   )
-# save(results, file = "R/sim_results.RData")
+save(res_twofac, file = "R/sim_results_twofac.RData")
