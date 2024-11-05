bdba8b2b7184b34d0b7d33631cf200b7977623a0
Haziq Jamil <haziq.jamil@ubd.edu.bn>
2024-11-05 19:46:32 +0800

    Add show and summary method for brlavaan

---
+++ R/00-class.R
@@ -0,0 +1,26 @@
+setClass(
+  Class = "brlavaan",
+  contains = "lavaan"
+)
+
+setMethod("show", "brlavaan", function(object) {
+  cat("br")
+  callNextMethod()
+})
+
+setMethod("summary", "brlavaan", function(object, ...) {
+  class(object) <- "lavaan"
+  cat("br")
+  out <- capture.output(callNextMethod())
+
+  if (object@Options$estimator == "IBRMP")
+    penalty_report <-
+      "  Penalty                                         TRUE"
+  else
+    penalty_report <-
+      "  Penalty                                        FALSE"
+
+  out <- append(out, penalty_report, after = 3)
+  cat(out, sep = "\n")
+
+})
