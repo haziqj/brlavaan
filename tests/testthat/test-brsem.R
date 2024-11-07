8167b663b5cfd56b14c2d3515d941ba271d245eb
Haziq Jamil <haziq.jamil@ubd.edu.bn>
2024-11-07 20:34:44 +0800

    Add tests for brsem, brcfa, and brgrowth
    Related to #18.

---
+++ tests/testthat/test-brsem.R
@@ -0,0 +1,34 @@
+test_that("brsem works", {
+  mod <- txt_mod_twofac(0.8)
+  dat <- gen_data_twofac(n = 50, rel = 0.8)
+  fit <- brsem(mod, dat, estimator = "eBRM")
+  tmp <- capture.output(print(fit))
+  tmp <- capture.output(summary(fit))
+  expect_true(is(fit, "brlavaan"))
+  expect_true(is(coef(fit), "lavaan.vector"))
+})
+
+test_that("brcfa works", {
+  HS.model <- "
+    visual  =~ x1 + x2 + x3
+    # textual =~ x4 + x5 + x6
+    # speed   =~ x7 + x8 + x9
+  "
+
+  fit <- brcfa(HS.model, data = HolzingerSwineford1939, estimator = "iBRMp")
+  tmp <- capture.output(print(fit))
+  tmp <- capture.output(summary(fit))
+  expect_true(is(fit, "brlavaan"))
+  expect_true(is(coef(fit), "lavaan.vector"))
+})
+
+test_that("brgrowth works", {
+  mod <- txt_mod_growth(0.5)
+  dat <- gen_data_growth(n = 50)
+  fit <- brgrowth(mod, dat, estimator = "ML")
+  tmp <- capture.output(print(fit))
+  tmp <- capture.output(summary(fit))
+  expect_true(is(fit, "brlavaan"))
+  expect_true(is(coef(fit), "lavaan.vector"))
+})
+
