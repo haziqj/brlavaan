682ee38303e242cba50857c92b9cb7b5b780f747
Haziq Jamil <haziq.jamil@ubd.edu.bn>
2024-12-03 14:47:04 +0800

    Add predicates and pretty print/summary methods

--- R/00-class.R
+++ R/00-class.R
@@ -9,24 +9,60 @@ setClass(
   contains = "lavaan"
 )
 
-setMethod("show", "brlavaan", function(object) {
-  cat("br")
-  callNextMethod()
-})
+rbmpen_report <- function(object) {
+  if (is_ML(object)) {
+    rbm_report <-
+      "  Bias reduction method                           NONE"
+    penalty_report <-
+      "  Plugin penalty                                  NONE"
+  } else if (is_eRBM(object)) {
+    rbm_report <-
+      "  Bias reduction method                       EXPLICIT"
+    penalty_report <-
+      "  Plugin penalty                                  NONE"
+  } else if (is_iRBM(object)) {
+    rbm_report <-
+      "  Bias reduction method                       IMPLICIT"
+    penalty_report <-
+      "  Plugin penalty                                  NONE"
+  } else {
+    tmp <- is_iRBMp(object, quietly = TRUE)
+    plugin_penalty <- attr(tmp, "plugin_penalty")
+    rbm_report <-
+      "  Bias reduction method                       IMPLICIT"
+    penalty_report <-
+      str_pad(plugin_penalty, 54 - nchar("  Plugin penalty "), side = "left")
+    penalty_report <- sprintf("  Plugin penalty %s", penalty_report)
+  }
 
-setMethod("summary", "brlavaan", function(object, ...) {
+  list(rbm_report = rbm_report, penalty_report = penalty_report)
+}
+
+setMethod("show", "brlavaan", function(object) {
+
+  rbm_report <- rbmpen_report(object)$rbm_report
+  penalty_report <- rbmpen_report(object)$penalty_report
+
   class(object) <- "lavaan"
   cat("br")
   out <- capture.output(callNextMethod())
+  out <- out[seq_len(length(out) / 2)]  # not sure why there are duplicates
+  out <- append(out, rbm_report, after = 3)
+  out <- append(out, penalty_report, after = 4)
+  cat(out, sep = "\n")
 
-  if (object@Options$estimator == "IBRMP")
-    penalty_report <-
-      "  Penalty                                         TRUE"
-  else
-    penalty_report <-
-      "  Penalty                                        FALSE"
+})
 
-  out <- append(out, penalty_report, after = 3)
+setMethod("summary", "brlavaan", function(object, ...) {
+
+  rbm_report <- rbmpen_report(object)$rbm_report
+  penalty_report <- rbmpen_report(object)$penalty_report
+
+  class(object) <- "lavaan"
+  cat("br")
+  out <- capture.output(callNextMethod())
+  out <- append(out, rbm_report, after = 3)
+  out <- append(out, penalty_report, after = 4)
   cat(out, sep = "\n")
 
 })
