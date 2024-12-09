5c9a8cf1d2ddb501eebdc0552cc1810c9cdb17ce
Haziq Jamil <haziq.jamil@ubd.edu.bn>
2024-12-09 16:05:01 +0800

    Add Huber penalty

--- R/12-plugins.R
+++ R/12-plugins.R
@@ -1,8 +1,12 @@
-pen_ridge <- function(x, lb, ub) {
+pen_ridge <- function(x, call = FALSE, ...) {
+  if (isTRUE(call)) return("Ridge penalty")
   sum(x ^ 2)
 }
 
-pen_ridge_bound <- function(x, lb, ub) {
+pen_ridge_bound <- function(x, lb, ub, call = FALSE, ...) {
+
+  if (isTRUE(call)) return("Bounded ridge penalty")
+
   fb <- which(!(is.infinite(lb) | is.infinite(ub)))
 
   below_penalty <- ifelse(x < lb, (lb - x) ^ 2, 0)
@@ -10,3 +14,43 @@ pen_ridge_bound <- function(x, lb, ub) {
 
   sum(below_penalty[fb] + above_penalty[fb])
 }
+
+# From IK 7/12/24
+#
+# huber <- function(x, thres = 1) {
+#   ifelse(abs(x) < thres, -0.5 * x^2, thres * (thres / 2 - abs(x)))
+# }
+#
+# huber_doubly_bounded <- function(x, low, upp, thres = 1) {
+#   x <- log((x - low) / (upp - x))
+#   huber(x, thres = thres)
+# }
+#
+# huber_positive <- function(x, low = 0, thres = 1) {
+#   x <- log(x - low)
+#   huber(x, thres = thres)
+# }
+
+pen_huber <- function(x, lb, ub, thres = 1, call = FALSE, ...) {
+
+  if (isTRUE(call)) return("Huber penalty")
+
+  y <-
+    ifelse(
+      is.finite(lb),
+      ifelse(
+        is.finite(ub),
+        log((x - lb) / (ub - x)),  # both lb and ub finite
+        log(x - lb)  # only lb finite
+      ),
+      0  # both lb and ub infinite
+    )
+
+  # y <- log(y)
+  y <- y[is.finite(y)]
+  sum(ifelse(abs(y) < thres, -0.5 * x ^ 2, thres * (thres / 2 - abs(y))))
+}
+
+# x <- c(275, 0, 50, 0, 20, 953)
+# lb <- c(0, -Inf, 0, -Inf, -10694, -40)
+# ub <- c(10694, Inf, 10694, Inf, 10694, 953)
