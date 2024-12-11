61978dfec4e81d676a074d641aa4b95163f4f830
Haziq Jamil <haziq.jamil@ubd.edu.bn>
2024-12-11 14:13:54 +0800

    Add tests for simulation functions

---
+++ tests/testthat/test-sim_fun.R
@@ -0,0 +1,27 @@
+test_that("Two-factor simulation function works", {
+  expect_no_error({
+    res <- sim_fun(
+      dist = "Normal",
+      model = "twofac",
+      rel = 0.8,
+      n = 100,
+      nsimu = 1,
+      lavsim = TRUE,
+      lavfun = "sem"
+    )
+  })
+})
+
+test_that("Growth simulation function works", {
+  expect_no_error({
+    res <- sim_fun(
+      dist = "Kurtosis",
+      model = "growth",
+      rel = 0.5,
+      n = 50,
+      nsimu = 1,
+      lavsim = FALSE,
+      lavfun = "growth"
+    )
+  })
+})
