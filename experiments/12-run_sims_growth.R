2d54b60f9ba1d5ca15f235b5df3c0ce174304766
Haziq Jamil <haziq.jamil@ubd.edu.bn>
2025-03-02 07:49:39 +0800

    sims growth script

--- experiments/12-run_sims_growth.R
+++ experiments/12-run_sims_growth.R
@@ -1,10 +1,11 @@
 simu_res_growth <- vector("list", length = nrow(simu_id))
-for (i in seq_len(nrow(simu_id))) {
+for (i in 1:10) {  # seq_len(nrow(simu_id))
   dist  <- simu_id$dist[i]
   model <- "growth"
   rel   <- simu_id$rel[i]
   n     <- simu_id$n[i]
-  seeds <- simu_id$seed[[i]][1:B]
+  # seeds <- simu_id$seed[[i]][1:B]
+  seeds <- NULL
 
   cli::cli_inform(">>> {Sys.time()} <<<\n\n[{i} / {nrow(simu_id)}] Now running {model} models ({dist}) rel = {rel}, n = {n}\n")
   simu_res_growth[[i]] <- sim_fun(
@@ -19,9 +20,9 @@ for (i in seq_len(nrow(simu_id))) {
     info_bias = "observed",
     info_se = "observed",
     seeds = seeds,
-    keep_going = FALSE,
+    keep_going = TRUE,
     data_scale = 1 / 10
   )
   cat("\n")
-  save(simu_res_growth, file = "experiments/simu_res_growth.RData")
+  save(simu_res_growth, file = "experiments/simu_res_growthmp2.RData")
 }
